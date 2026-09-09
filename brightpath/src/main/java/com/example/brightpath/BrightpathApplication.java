e487be72ecfcbb2a4aea4702520426bc96fda99f
ToilaChi <chi12345pham@gmail.com>
2026-09-09 15:24:48 +0700

    Initial commit

---
+++ brightpath/src/main/java/com/example/brightpath/BrightpathApplication.java
@@ -0,0 +1,13 @@
+package com.example.brightpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrightpathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrightpathApplication.class, args);
+	}
+
+}
---
+++ brightpath/src/test/java/com/example/brightpath/BrightpathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brightpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrightpathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
